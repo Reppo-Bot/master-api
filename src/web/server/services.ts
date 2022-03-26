013273f13f8199d50293c3b6fcd2e7fd7c4b2a26
17robots <mdray@ameritech.net>
2022-03-26 14:08:06 -0400

    feat: add server services

--- src/web/server/services.ts
+++ src/web/server/services.ts
@@ -1,44 +1,87 @@
 import { PrismaClient } from '@prisma/client'
+import { Config, Server } from './types'
 
-const getServer = async (serverid: string) => {
+const getServer = async (serverid: string): Promise<Server> => {
     const prisma = new PrismaClient()
-    try {
-        // we need to get these details for the server:
-    } catch(e) {
-
-    }
+    const bot = await prisma.bot.findUnique({
+        where: {
+            serverid: serverid
+        }
+    })
+    if(!bot) throw new Error('failed to grab bot')
+    const { name, bio }: Config = bot.config as unknown as Config
+    const { serveravatar } = bot
+    return { name, bio, avatar: serveravatar }
 }
 
-const getTopUsers = async () => {
-    try {
-        console.log('hello reps')
-    } catch(e) {
-
-    }
+const getTopUsers = async (serverid: string, num: number) => {
+    const prisma = new PrismaClient()
+    const users = await prisma.user.findMany({
+        where: {
+            Rep: {
+                some: {
+                    serverid: serverid
+                },
+            }
+        },
+        select: {
+            id: true,
+            discordid: true,
+            Rep: {
+                select: {
+                    rep: true
+                },
+                orderBy: {
+                    rep: 'desc'
+                }
+            }
+        },
+        take: num
+    })
+    if(users == null) throw new Error('failed to grab users')
+    return users
 }
 
-const getActivityForDay = async () => {
-    try {
-        console.log('hello activity for day')
-    } catch(e) {
-
-    }
+const getActivityForDay = async (serverid: string) => {
+    const prisma = new PrismaClient()
+    const transactions = await prisma.transaction.findMany({
+        where: {
+            serverid: serverid,
+            time: {
+                gte: new Date(new Date().setDate(new Date().getDate() - 1))
+            }
+        }
+    })
+    if(transactions == null) throw new Error('failed to grab transactions')
+    return transactions
 }
 
-const getActivityForMonth = async () => {
-    try {
-        console.log('hello activity for month')
-    } catch(e) {
-
-    }
+const getActivityForMonth = async (serverid: string) => {
+    const prisma = new PrismaClient()
+    const transactions = await prisma.transaction.findMany({
+        where: {
+            serverid: serverid,
+            time: {
+                gte: new Date(new Date().setMonth(new Date().getMonth() - 1))
+            }
+        }
+    })
+    if(transactions == null) throw new Error('failed to grab transactions')
+    return transactions
 }
 
-const getActivityForYear = async () => {
-    try {
-        console.log('hello activity for year')
-    } catch(e) {
-
-    }
+const getActivityForYear = async (serverid: string) => {
+    const prisma = new PrismaClient()
+    const transactions = await prisma.transaction.findMany({
+        where: {
+            serverid: serverid,
+            time: {
+                gte: new Date(new Date().setFullYear(new Date().getFullYear() - 1))
+            }
+        }
+    })
+    if(transactions == null) throw new Error('failed to grab transactions')
+    return transactions
 }
 
 export default {
---
+++ src/web/server/types.ts
@@ -0,0 +1,12 @@
+export interface Server {
+    name: string,
+    bio: string,
+    avatar: string
+}
+
+export interface Config {
+    serverId: string
+    defaultRep: number
+    name: string
+    bio: string
+}
