eafc67662bba19340c2d61cf5af6b0ec0cc9ec3f
17robots <mdray@ameritech.net>
2022-03-26 18:19:58 -0400

    feat: add server controllers

--- src/web/server/controller.ts
+++ src/web/server/controller.ts
@@ -1,48 +1,54 @@
 import { Request, Response, NextFunction } from "express"
+import { success, fail } from "../../util"
 import serverService from './services'
 
 const getServer = async (req: Request, res: Response, next: NextFunction) => {
+    const { serverid } = req.body
     try {
-        const server = await serverService.getServer()
-        return server
+        const server = await serverService.getServer(serverid)
+        success(server, res, next)
     } catch(e) {
-
+        fail(e, res)
     }
 }
 
 const getTopUsers = async (req: Request, res: Response, next: NextFunction) => {
+    const { serverid, num } = req.body
     try {
-        const topUsers = await serverService.getTopUsers()
-        return topUsers
+        const topUsers = await serverService.getTopUsers(serverid, num)
+        success(topUsers, res, next)
     } catch(e) {
-
+        fail(e, res)
     }
 }
 
 const getActivityForDay = async (req: Request, res: Response, next: NextFunction) => {
+    const { serverid } = req.body
     try {
-        const activityForDay = await serverService.getActivityForDay()
-        return activityForDay
+        const activityForDay = await serverService.getActivityForDay(serverid)
+        success(activityForDay, res, next)
     } catch(e) {
-
+        fail(e, res)
     }
 }
 
 const getActivityForMonth = async (req: Request, res: Response, next: NextFunction) => {
+    const { serverid } = req.body
     try {
-        const activityForMonth = await serverService.getActivityForMonth()
-        return activityForMonth
+        const activityForMonth = await serverService.getActivityForMonth(serverid)
+        success(activityForMonth, res, next)
     } catch(e) {
-
+        fail(e, res)
     }
 }
 
 const getActivityForYear = async (req: Request, res: Response, next: NextFunction) => {
+    const { serverid } = req.body
     try {
-        const activityForYear = await serverService.getActivityForYear()
-        return activityForYear
+        const activityForYear = await serverService.getActivityForYear(serverid)
+        success(activityForYear, res, next)
     } catch(e) {
-
+        fail(e, res)
     }
 }
 
