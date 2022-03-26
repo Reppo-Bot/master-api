97eb9bbe357895cd7b78480d5468962c2aab10db
17robots <mdray@ameritech.net>
2022-03-26 18:25:41 -0400

    feat: add user controllers

--- src/web/user/controller.ts
+++ src/web/user/controller.ts
@@ -1,57 +1,64 @@
 import { Request, Response, NextFunction } from 'express'
+import { success, fail } from '../../util' 
 import userService from './services'
 
 const getUser = async (req: Request, res: Response, next: NextFunction) => {
+    const { discordid } = req.body
     try {
-        const user = await userService.getUser()
-        return user
+        const user = await userService.getUser(discordid)
+        success(user, res, next)
     } catch(e) {
-
+        fail(e, res)
     }
 }
 
 const getReps = async (req: Request, res: Response, next: NextFunction) => {
+    const { userid } = req.body
     try {
-        const reps = await userService.getReps()
-        return reps
+        const reps = await userService.getReps(userid)
+        success(reps, res, next)
     } catch(e) {
-
+        fail(e, res)
     }
 }
 
 const getRecentTransactions = async (req: Request, res: Response, next: NextFunction) => {
+    const { userid, num } = req.body
     try {
-        const recentTransactions = await userService.getRecentTransactions()
-        return recentTransactions
+        const recentTransactions = await userService.getRecentTransactions(userid, num)
+        success(recentTransactions, res, next)
     } catch(e) {
-
+        fail(e, res)
     }
 }
 
 const getActivityForDay = async (req: Request, res: Response, next: NextFunction) => {
+    const { userid } = req.body
     try {
-        const activityForDay = await userService.getActivityForDay()
-        return activityForDay
+        const activityForDay = await userService.getActivityForDay(userid)
+        success(activityForDay, res, next)
     } catch(e) {
-
+        fail(e, res)
     }
 }
 
 const getActivityForMonth = async (req: Request, res: Response, next: NextFunction) => {
+    const { userid } = req.body
     try {
-        const activityForMonth = await userService.getActivityForMonth()
-        return activityForMonth
+        const activityForMonth = await userService.getActivityForMonth(userid)
+        success(activityForMonth, res, next)
     } catch(e) {
-
+        fail(e, res)
     }
 }
 
 const getActivityForYear = async (req: Request, res: Response, next: NextFunction) => {
+    const { userid } = req.body
     try {
-        const activityForYear = await userService.getActivityForYear()
-        return activityForYear
+        const activityForYear = await userService.getActivityForYear(userid)
+        success(activityForYear, res, next)
     } catch(e) {
-
+        fail(e, res)
     }
 }
 
