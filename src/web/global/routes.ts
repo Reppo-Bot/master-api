dd6274fd8467059442acb56dcaae784e0dd60c02
17robots <mdray@ameritech.net>
2022-03-31 21:27:59 -0400

    fix: add login controller to routes

--- src/web/global/routes.ts
+++ src/web/global/routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
+import globalController from './controller'
 
 const router = Router()
 
-router.post('/login')
+router.post('/login', globalController.login)
 
 export default router
