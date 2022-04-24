541d994225aa044ab6252a29fcfa7db2958f5bb0
= <=>
2022-04-24 19:13:49 -0400

    feat: add servername and command name to get recent transactions

--- src/web/user/services.ts
+++ src/web/user/services.ts
@@ -21,17 +21,31 @@ const getReps = async (userid: string) => {
 
 const getRecentTransactions = async (userid: string, num: number) => {
     const prisma = new PrismaClient()
+    const user = await prisma.user.findUnique({ where: { discordid: userid }})
+    if(!user) throw new Error('Failed to find specified user')
     if(num <= 0) throw new Error("Please specify a number greater than 0")
     const transactions = await prisma.transaction.findMany({
         where: {
             OR: [
                 {
-                    senderid: userid
+                    senderid: user.id
                 },
                 {
-                    receiverid: userid
+                    receiverid: user.id
                 }
             ],
+        },
+        include: {
+            action: {
+                select: {
+                    commandname: true
+                }
+            },
+            Bot: {
+                select: {
+                    servername: true
+                }
+            }
         },
         take: num,
         orderBy: {
@@ -45,21 +59,36 @@ const getRecentTransactions = async (userid: string, num: number) => {
 
 const getActivityForDay = async (userid: string) => {
     const prisma = new PrismaClient()
+    const user = await prisma.user.findUnique({ where: { discordid: userid }})
+    if(!user) throw new Error('Failed to find specified user')
     const transactions = await prisma.transaction.findMany({
         where: {
             OR: [
                 {
-                    senderid: userid
+                    senderid: user.id
                 },
                 {
-                    receiverid: userid
+                    receiverid: user.id
                 }
             ],
             time: {
                 gte: new Date(new Date().setDate(new Date().getDate() - 1))
             }
+        },
+        include: {
+            action: {
+                select: {
+                    commandname: true
+                }
+            },
+            Bot: {
+                select: {
+                    servername: true
+                }
+            }
         }
     })
+    const actionIds = transactions.map((transaction)=> transaction.actionid)
     if(transactions == null) throw new Error('failed to grab transactions')
     await prisma.$disconnect()
     return transactions
@@ -67,18 +96,32 @@ const getActivityForDay = async (userid: string) => {
 
 const getActivityForMonth = async (userid: string) => {
     const prisma = new PrismaClient()
+    const user = await prisma.user.findUnique({ where: { discordid: userid }})
+    if(!user) throw new Error('Failed to find specified user')
     const transactions = await prisma.transaction.findMany({
         where: {
             OR: [
                 {
-                    senderid: userid
+                    senderid: user.id
                 },
                 {
-                    receiverid: userid
+                    receiverid: user.id
                 }
             ],
             time: {
                 gte: new Date(new Date().setMonth(new Date().getMonth() - 1))
+            }
+        },
+        include: {
+            action: {
+                select: {
+                    commandname: true
+                }
+            },
+            Bot: {
+                select: {
+                    servername: true
+                }
             }
         }
     })
@@ -89,18 +132,32 @@ const getActivityForMonth = async (userid: string) => {
 
 const getActivityForYear = async (userid: string) => {
     const prisma = new PrismaClient()
+    const user = await prisma.user.findUnique({ where: { discordid: userid }})
+    if(!user) throw new Error('Failed to find specified user')
     const transactions = await prisma.transaction.findMany({
         where: {
             OR: [
                 {
-                    senderid: userid
+                    senderid: user.id
                 },
                 {
-                    receiverid: userid
+                    receiverid: user.id
                 }
             ],
             time: {
                 gte: new Date(new Date().setFullYear(new Date().getFullYear() - 1))
+            }
+        },
+        include: {
+            action: {
+                select: {
+                    commandname: true
+                }
+            },
+            Bot: {
+                select: {
+                    servername: true
+                }
             }
         }
     })
